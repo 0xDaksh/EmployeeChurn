d25a2afb2e4b99b669da02513729464415fbb57a
Daksh Miglani <hello@dak.sh>
2017-05-25 19:59:31 +0530

    predicted the result with 95% accuracy

--- clf.py
+++ clf.py
@@ -38,3 +38,27 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.1, random_
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+# Libraries
+from keras.models import Sequential
+from keras.layers import Dense
+# creating the model
+clf = Sequential([
+    Dense(units=8, kernel_initializer='uniform', activation='relu', input_dim=10),
+    Dense(units=9, kernel_initializer='uniform', activation='relu'), # units are based on my creativity :3
+    Dense(1, kernel_initializer='uniform', activation='sigmoid') #output
+])
+
+# compiling the model
+clf.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# training the model
+clf.fit(X_train, Y_train, batch_size=9, epochs=100)
+
+# predicting the values
+Y_pred = clf.predict(X_test)
+Y_pred = (Y_pred > 0.5)
+
+# Evaluate model
+score = clf.evaluate(X_test, Y_test, batch_size=128)
+print(score) # 95% or 0.9586
